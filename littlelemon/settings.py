fe76857320470b6e3c34d2d82077f081f24fc732
Michael M.  Robinson <michael.maurce.robinson@gmail.com>
2023-11-22 20:10:43 -0500

    Added Menu and Booking models to project. Also setup Django's admin interface.

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -75,10 +75,18 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#Settings.py
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 'root',
+        'PASSWORD': 'tm2@pw0nn!@#$',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
