a0665f626472aab8f804485b09bd7fbabe98209c
Michael Maurice Robinson <127051533+michael-m-robinson@users.noreply.github.com>
2023-11-24 14:17:36 -0500

    Added Django REST API Framwork
    - Added Django REST Framework ✅
    - Added Menu API ✅
    - Added Booking API ✅

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from .models import Menu, Booking
+from django.contrib.auth.models import User
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
